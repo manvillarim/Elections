27d570435a4adf0a201583987bedfabd48a94f4b
manvillarim <manvillarim@gmail.com>
2024-12-15 06:40:15 -0300

    time system

--- src/Election.sol
+++ src/Election.sol
@@ -8,8 +8,10 @@ contract Election{
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
 
+    event StartElection(uint256 time, address owner);
     event Vote(address voter, address candidate);
     event AddCandidate(address candidate);
+    event CloseElection(uint256 time);
 
     /*//////////////////////////////////////////////////////////////
                              ERRORS
@@ -21,6 +23,8 @@ contract Election{
     error CandidateIsAddressZero();
     error SenderIsAddressZero();
     error SenderIsNotOwner();
+    error ElectionHasNotStarted();
+    error ElectionAlreadyStarted();
 
     /*//////////////////////////////////////////////////////////////
                              STORAGE VARIABLES
@@ -33,23 +37,27 @@ contract Election{
     address[] public s_candidates;
     address private s_winner;
     address private s_owner;
+
     uint256 private s_maxVotes;
+    bool s_startElection;
+    uint256 s_durationTime;
+    uint256 s_endTime;
 
     /*//////////////////////////////////////////////////////////////
                                CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
-    constructor(address[] memory candidates, address owner) {
+    constructor(address[] memory candidates, uint256 time) {
 
         if (candidates.length <= 0) {
             revert NoCandidates();
         }
 
-        if(owner == address(0)) {
+        if(msg.sender == address(0)) {
             revert SenderIsAddressZero();
         }
 
-        s_owner = owner;
+        s_owner = msg.sender;
 
         for (uint256 i = 0; i < candidates.length; i++) {
             if(candidates[i] == address(0)) {
@@ -60,6 +68,8 @@ contract Election{
         }
 
         s_candidates = candidates;
+        s_startElection = false;
+        s_durationTime = time;
     }
 
 
@@ -69,28 +79,44 @@ contract Election{
     
     modifier safeCandidate(address candidate) {
 
-        if (!s_isCandidate[candidate]) {
+        if (!s_isCandidate[candidate])
             revert CandidateDoesntExist(candidate);
-        }
 
-        if(candidate == address(0)) {
+        if(candidate == address(0))
             revert CandidateIsAddressZero();
-        }
         _;
     }
 
     modifier safeSender() {
         
-        if(msg.sender == address(0)) {
+        if(msg.sender == address(0))
             revert SenderIsAddressZero();
-        }
         _;
 
     }
 
     modifier onlyOwner() {
-        if(msg.sender != s_owner) {
+        if(msg.sender != s_owner)
             revert SenderIsNotOwner();
+        _;
+    }
+
+    modifier onlyDuringElection() {
+
+        if(block.timestamp >= s_endTime) {
+            emit CloseElection(s_endTime);
+            s_startElection = false;
+        }
+
+        if(!s_startElection) {
+            revert ElectionHasNotStarted();
+        }
+        _;
+    }
+
+    modifier onlyBeforeElection() {
+        if(s_startElection) {
+            revert ElectionAlreadyStarted();
         }
         _;
     }
@@ -99,7 +125,23 @@ contract Election{
                             ELECTION LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function vote(address candidate) external safeCandidate(candidate) safeSender() {
+    function startElection() external onlyOwner onlyBeforeElection{
+
+        s_endTime = block.timestamp + s_durationTime;
+        s_startElection = true;
+
+        emit StartElection(block.timestamp, s_owner);
+
+    }
+
+    function endElection() external onlyOwner {
+
+        s_startElection = false;
+
+        emit CloseElection(block.timestamp);
+    }
+
+    function vote(address candidate) external safeCandidate(candidate) safeSender onlyDuringElection {
 
         if (s_hasVoted[msg.sender]) {
             revert VoterAlreadyVoted(msg.sender);
@@ -113,11 +155,7 @@ contract Election{
         updateWinner(candidate);
     }
 
-    function addCandidate(address candidate) external onlyOwner{
-        
-        if(candidate == address(0)) {
-            revert CandidateIsAddressZero();
-        }
+    function addCandidate(address candidate) external onlyOwner safeCandidate(candidate) onlyBeforeElection{
         
         s_isCandidate[candidate] = true;
         s_candidateVotes[candidate] = 0;
@@ -139,6 +177,7 @@ contract Election{
 
         return (s_candidates, votes);
     }
+
     function getWinner() external view returns(address) {
         return s_winner;
     }
@@ -155,6 +194,10 @@ contract Election{
         return s_candidateVotes[candidate];
     }
 
+    function getTime() external view returns(uint256) {
+        return block.timestamp;
+    }
+
     /*//////////////////////////////////////////////////////////////
                             INTERNAL LOGIC
     //////////////////////////////////////////////////////////////*/
@@ -167,4 +210,5 @@ contract Election{
         } 
 
     }
+
 }
